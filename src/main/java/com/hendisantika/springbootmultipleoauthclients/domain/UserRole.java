2d773fb37b8ca8dcc0289474280dca8c654c90d2
hendisantika <hendisantika@yahoo.co.id>
2019-03-17 20:39:00 +0700

    #3 - Add User Role Domain

---
+++ src/main/java/com/hendisantika/springbootmultipleoauthclients/domain/UserRole.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootmultipleoauthclients.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-multiple-oauth-clients
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-03-17
+ * Time: 20:38
+ */
+public enum UserRole {
+    USER,
+    PLAYER,
+    ADMINISTRATOR
+}
