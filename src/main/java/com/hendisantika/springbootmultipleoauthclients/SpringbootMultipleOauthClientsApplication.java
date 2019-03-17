ccdcf8da0dc89a47261e896d6ceeb5b13cc80967
hendisantika <hendisantika@yahoo.co.id>
2019-03-17 20:36:35 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootmultipleoauthclients/SpringbootMultipleOauthClientsApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootmultipleoauthclients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMultipleOauthClientsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMultipleOauthClientsApplication.class, args);
+    }
+
+}
