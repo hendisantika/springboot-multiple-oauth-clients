cdbde7a5c5d85f4681d2b32c210842cff1572380
hendisantika <hendisantika@yahoo.co.id>
2019-03-17 20:46:33 +0700

    #11 - Modify Main Class

--- src/main/java/com/hendisantika/springbootmultipleoauthclients/SpringbootMultipleOauthClientsApplication.java
+++ src/main/java/com/hendisantika/springbootmultipleoauthclients/SpringbootMultipleOauthClientsApplication.java
@@ -2,12 +2,27 @@ package com.hendisantika.springbootmultipleoauthclients;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @SpringBootApplication
 public class SpringbootMultipleOauthClientsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootMultipleOauthClientsApplication.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
     }
 
 }
